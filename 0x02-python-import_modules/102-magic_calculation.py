4979879271123e9f698f91f6a878808076107495
Abraham <awoleyeabrahamseyi@gmail.com>
2024-09-12 19:00:48 +0100

    Magic Calculation

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
-
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
+    from magic_calculation_102 import add, sub  # Import functions
 
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
+        c = add(a, b)  # Call add if a is less than b
+        for i in range(4, 6):  # Loop 4 times, i from 4 to 5
+            c = add(c, i)  # Add i to c in each loop iteration
+        return c  # Return the final value of c
     else:
-        return(sub(a, b))
+        return sub(a, b)  # Call sub if a is not less than b
+
+    # This line (LOAD_CONST None) is unreachable in the bytecode
+    # due to the previous return statements. We can omit it.
