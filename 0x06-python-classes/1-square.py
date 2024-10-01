f09ba4a7c6721d1086fc8aba958198f5d33b2f35
Abraham <awoleyeabrahamseyi@gmail.com>
2024-10-01 16:45:41 +0100

    Added up to 4-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This is used to Define a class Square."""
+
+
+class Square:
+    """To Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
